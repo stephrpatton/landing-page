a4e0482289e574ec4d5976e476a0a8ba4738ce89
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-14 11:56:10 -0500

    fixed mobile view font and testimonal section

--- JS/testimonial.js
+++ JS/testimonial.js
@@ -2,24 +2,37 @@ class Testimonials {
   constructor(test) {
     this.test = test;
     // console.log(test);
-    this.testExpand = test.querySelector(".content-header");
+    this.testHeader = test.querySelector(".content-header");
     this.button = test.querySelector(".see-more");
     this.testPara = test.querySelector(".content-para");
     this.testImg = test.querySelector(".content-img");
     this.button.addEventListener("click", () => this.openTest());
-    this.test.addEventListener("mouseleave", () => this.closeTest());
+    // this.test.addEventListener("mouseleave", () => this.closeTest());
   }
 
   openTest() {
-    TweenMax.to(this.testPara, 0.5, { opacity: 1 });
-    TweenMax.to(this.button, 0.5, { opacity: 0 });
+    if ((this.testPara.style.opacity = "0")) {
+      TweenMax.to(this.button, 0.2, { display: "none" });
+      TweenMax.to(this.testPara, 0.5, {
+        height: "50px",
+        opacity: 1,
+        delay: 1
+      });
+      TweenMax.to(this.testImg, 0.6, {
+        display: "inline-block",
+        paddingRight: "3rem"
+      });
+      TweenMax.to(this.test, 0.5, { width: "80%" });
+
+      // TweenMax.to(this.testHeader, 0.2, { x: -190, delay: 0.8 });
+    }
   }
 
-  closeTest() {
-    TweenMax.to(this.testPara, 0.5, { opacity: 0 });
+  // closeTest() {
+  //   TweenMax.to(this.testPara, 0.5, { opacity: 0 });
 
-    TweenMax.to(this.button, 0.5, { opacity: 1 });
-  }
+  //   TweenMax.to(this.button, 0.5, { opacity: 1 });
+  // }
 }
 
 let testimonialContent = document.querySelectorAll(".testimonial-content");
