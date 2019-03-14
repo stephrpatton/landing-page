6cf45fe434b3baf9401dba56bdbf0ed52a5aee94
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-14 13:04:49 -0500

    fixed mobile view for testimonial and about page

--- JS/testimonial.js
+++ JS/testimonial.js
@@ -1,38 +1,31 @@
 class Testimonials {
   constructor(test) {
     this.test = test;
-    // console.log(test);
     this.testHeader = test.querySelector(".content-header");
     this.button = test.querySelector(".see-more");
     this.testPara = test.querySelector(".content-para");
     this.testImg = test.querySelector(".content-img");
+
+    //Click Event
     this.button.addEventListener("click", () => this.openTest());
-    // this.test.addEventListener("mouseleave", () => this.closeTest());
   }
 
   openTest() {
     if ((this.testPara.style.opacity = "0")) {
       TweenMax.to(this.button, 0.2, { display: "none" });
-      TweenMax.to(this.testPara, 0.5, {
-        height: "50px",
+      TweenMax.to(this.testPara, 0.8, {
+        height: "auto",
         opacity: 1,
-        delay: 1
+        delay: 0.5
       });
-      TweenMax.to(this.testImg, 0.6, {
+
+      TweenMax.to(this.testImg, 0.8, {
         display: "inline-block",
         paddingRight: "3rem"
       });
-      TweenMax.to(this.test, 0.5, { width: "80%" });
-
-      // TweenMax.to(this.testHeader, 0.2, { x: -190, delay: 0.8 });
+      TweenMax.to(this.test, 0.4, { width: "80%" });
     }
   }
-
-  // closeTest() {
-  //   TweenMax.to(this.testPara, 0.5, { opacity: 0 });
-
-  //   TweenMax.to(this.button, 0.5, { opacity: 1 });
-  // }
 }
 
 let testimonialContent = document.querySelectorAll(".testimonial-content");
